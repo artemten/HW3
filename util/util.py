b314696daf40f8babdab009c0c45d5167b0d75a4
artemten <artemten81@gmail.com>
2022-12-23 19:46:15 +0300

    Made another changes

--- util/util.py
+++ util/util.py
@@ -1,3 +1,9 @@
 # here is pickle 
+import pickle
 
-    
+
+def save_model(dir: str, model) -> None:
+    pickle.dump(model, open(dir, 'wb'))
+
+def load_model(dir: str) -> None:
+    return pickle.load(open(dir, 'rb'))
